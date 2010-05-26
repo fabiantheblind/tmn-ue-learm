ce38dc47cc4fc63b40135a211a72ae957c0201f1
sekan.pete <sekan.pete@c7aafc91-a54f-9bc5-4ece-305c0ad91397>
2010-05-26 10:47:07 +0000

    upload 01

---
+++ trunk/tmn_UE_Laerm/src/tmnuelaerm/TmnUELaerm.java
@@ -0,0 +1,22 @@
+package tmnuelaerm;
+
+import processing.core.PApplet;
+
+
+public class TmnUELaerm extends PApplet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1217858472488377688L;
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { tmnuelaerm.TmnUELaerm.class.getName() });
+	}
+}
