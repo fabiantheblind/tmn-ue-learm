72c963df627a2b62b75f0450cea78e8999fe1e3d
fabiantheblind <fabiantheblind@c7aafc91-a54f-9bc5-4ece-305c0ad91397>
2010-07-11 14:53:15 +0000

    index is public

--- trunk/tmn_UE_Laerm/src/particleSystem/Property.java
+++ trunk/tmn_UE_Laerm/src/particleSystem/Property.java
@@ -3,7 +3,7 @@ package particleSystem;
 /**
  * these are some propertys for repelling to get some sort of rules<br>
  * the data is loaded from an .xml file but can also be set manually for every
- * Property<br>
+ * {@code Property}<br>
  * its only for storing an {@code int, string,float[][]} with 6 values in an
  * <code>Object</code>
  * 
@@ -15,7 +15,13 @@ package particleSystem;
 public class Property {
 
 	/**
-	 * the index from the xml
+	 * a 2 dimensional float array to store the values
+	 * {@code float [time( 2 values )][space( 3 values )]} the time 0 day or 1
+	 * night the space as private 0 / public 1 / work 2
+	 */
+	int[][] affectionProps;
+	/**
+	 * the index from the .xml file
 	 * 
 	 */
 	public int index;
@@ -23,27 +29,6 @@ public class Property {
 	 * the word or name to write to the screen
 	 */
 	String name;
-	/**
-	 * a 2 dimensinal float array to store the values
-	 * {@code float [time( 2 values )][space( 3 values )]} the time 0 day or 1
-	 * night the space as private 0 / public 1 / work 2
-	 */
-	int[][] affectionProps;
-	
-	/**
-	 * @param index
-	 * @param name
-	 * @param affectionProps
-	 * @see #index
-	 * @see #name
-	 * @see #affectionProps
-	 */
-	public Property(int index, String name, int[][] affectionProps) {
-		this.index = index;
-		this.name = name.toUpperCase();
-		this.affectionProps = affectionProps;
-
-	}
 	
 	public Property() {
 		
@@ -60,7 +45,49 @@ public class Property {
 		this.affectionProps = propertys;
 
 	}
+	
+	/**
+	 * @param index
+	 * @param name
+	 * @param affectionProps
+	 * @see #index
+	 * @see #name
+	 * @see #affectionProps
+	 */
+	public Property(int index, String name, int[][] affectionProps) {
+		this.index = index;
+		this.name = name.toUpperCase();
+		this.affectionProps = affectionProps;
 
+	}
+
+	/**
+	 * @param extProp clone another {@code Property}
+	 */
+	public void cloneProperty(Property extProp){
+
+		
+		this.index = extProp.index;
+		this.name = extProp.name;
+		this.affectionProps = extProp.affectionProps;
+	}
+
+	/**
+	 * @return the affectionProps
+	 */
+	public synchronized int[][] getAffectionProps() {
+		return affectionProps;
+	}
+
+	/**
+	 * @return all daytime values from the Class {@code Property} as a {@code float[]}
+	 */
+	public int[] getDayValues() {
+		int[] dayValues = new int[] { affectionProps[0][0],
+				affectionProps[0][1], affectionProps[0][2] };
+		return dayValues;
+
+	}
 	/**
 	 * @return the index
 	 */
@@ -75,12 +102,17 @@ public class Property {
 		return name;
 	}
 
+
 	/**
-	 * @return the affectionProps
+	 * @return all nitetime values from the Class {@code Property} as a {@code float[]}
 	 */
-	public synchronized int[][] getAffectionProps() {
-		return affectionProps;
+	public int[] getNiteValues() {
+		int[] dayValues = new int[] { affectionProps[1][0],
+				affectionProps[1][1], affectionProps[1][2] };
+		return dayValues;
+
 	}
+
 	public synchronized void setAffectionProps(int[][] affectionProps) {
 		this.affectionProps = affectionProps;
 	}
@@ -93,25 +125,6 @@ public class Property {
 	}
 
 
-	/**
-	 * @return all daytime values from the Class Property as a {@code float[]}
-	 */
-	public int[] getDayValues() {
-		int[] dayValues = new int[] { affectionProps[0][0],
-				affectionProps[0][1], affectionProps[0][2] };
-		return dayValues;
-
-	}
-
-	/**
-	 * @return all nitetime values from the Class Property as a {@code float[]}
-	 */
-	public int[] getNiteValues() {
-		int[] dayValues = new int[] { affectionProps[1][0],
-				affectionProps[1][1], affectionProps[1][2] };
-		return dayValues;
-
-	}
 
 	/**
 	 * 
@@ -127,20 +140,6 @@ public class Property {
 	 */
 	public int valueByIndex(int time, int space) {
 		return affectionProps[time][space];
-	}
-
-	/**
-	 * @param affectionProps
-	 *            the affectionProps to set
-	 */
-
-	
-	public void cloneProperty(Property extProp){
-
-		
-		this.index = extProp.index;
-		this.name = extProp.name;
-		this.affectionProps = extProp.affectionProps;
 	}
 
 }
